d75109354109caba11783e8578816c19c9786e00
virusas3000 <your@email.com>
2026-06-06 01:52:24 +0800

    fix: main hero CTA button redirects to /neon-services/

--- wp-content/themes/neonlighthk/template-parts/section-hero.php
+++ wp-content/themes/neonlighthk/template-parts/section-hero.php
@@ -10,6 +10,6 @@
 		<h4 class="nl-hero__subtitle"><?php echo nl_t('hero_label'); ?></h4>
 		<h1 class="nl-hero__headline"><?php echo nl_t('hero_title'); ?></h1>
 		<h2 class="nl-hero__title"><?php echo nl_t('hero_subtitle'); ?></h2>
-		<a href="<?php echo esc_url( home_url( '/about/' ) ); ?>?lang=<?php echo nl_lang(); ?>" class="nl-hero__btn"><?php echo nl_t('hero_cta'); ?></a>
+		<a href="<?php echo esc_url( home_url( '/neon-services/' ) ); ?>?lang=<?php echo nl_lang(); ?>" class="nl-hero__btn"><?php echo nl_t('hero_cta'); ?></a>
 	</div>
 </section>
